a3f22c68e4b65b3fcf9bcd92ac1e35e1bb7d31a2
Hristina <hristina.atanasova6@gmail.com>
2020-04-27 20:45:53 +0200

    Set production environment true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyBIhyqPAtNMC0cPnoFfcY2j-8jVsOS1_Rw",
     authDomain: "budget-manager-ddea4.firebaseapp.com",
