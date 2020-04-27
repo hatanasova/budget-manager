e501d1c16f94bf7c48ffaac9e55661207f61fc27
Hristina <hristina.atanasova6@gmail.com>
2020-04-27 20:48:17 +0200

    Revert commit prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: "AIzaSyBIhyqPAtNMC0cPnoFfcY2j-8jVsOS1_Rw",
     authDomain: "budget-manager-ddea4.firebaseapp.com",
