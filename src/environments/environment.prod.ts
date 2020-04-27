23c8f29f229d0559e74461b779e69815f24145ff
Hristina <hristina.atanasova6@gmail.com>
2020-04-27 20:51:40 +0200

    Add firebase details in prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBIhyqPAtNMC0cPnoFfcY2j-8jVsOS1_Rw",
+    authDomain: "budget-manager-ddea4.firebaseapp.com",
+    databaseURL: "https://budget-manager-ddea4.firebaseio.com",
+    projectId: "budget-manager-ddea4",
+    storageBucket: "budget-manager-ddea4.appspot.com",
+    messagingSenderId: "402906155012",
+  }
 };
