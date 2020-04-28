bcfa9042891a4ea86bd94d1551964a4b081f5de4
Hristina <hristina.atanasova6@gmail.com>
2020-04-28 18:24:47 +0200

    Create landing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AngularFireModule } from '@angular/fire';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+  
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment'; 
 
@@ -11,10 +14,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { from } from 'rxjs';
+import { HeaderComponent } from './components/header/header.component';
+import { LandingContainerComponent } from './components/landing-container/landing-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingContainerComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +30,8 @@ import { from } from 'rxjs';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule, // for database
     FormsModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
